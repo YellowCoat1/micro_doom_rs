848f05594f522f98ed06a6ea5c7e582d6c4fa428
Enzo <enzomlg88@gmail.com>
2025-11-15 17:43:28 -0800

    WALLLLLLS

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -4,29 +4,75 @@ mod a3d_to_2d;
 mod lines;
 mod array;
 mod polygons;
+use ggez::GameError;
 use lines::LineSegment;
 use ggez::{Context, GameResult, event, graphics, graphics::Color};
 use ggez::graphics::{DrawMode, Mesh};
+use vecs::{Vec2, Vec3};
 
-use crate::game::vecs::Vec2;
+use polygons::Polygon;
+
 pub struct GameState {
+    camera3d: Vec3,
+    walls:Vec<Polygon>
 }
 
-
+fn wall_floor_to_3d(wall_left: &Vec2, wall_right: &Vec2) -> Vec<Vec3> {
+    let mut three_d_point = vec![];
+    let offset_up = 10.0;
+    three_d_point.push(Vec3 {
+        x: wall_left.x,
+        y: 1.0,
+        z: wall_left.y
+    });
+    three_d_point.push(Vec3 {
+        x: wall_left.x,
+        y: offset_up,
+        z: wall_left.y
+    });
+    three_d_point.push(Vec3 {
+        x: wall_right.x,
+        y: offset_up,
+        z: wall_right.y
+    });
+    three_d_point.push(Vec3 {
+        x: wall_right.x,
+        y: 1.0,
+        z: wall_right.y
+    });
+    three_d_point
+}
 
 impl GameState {
     pub fn new(ctx: &mut Context) -> Self {
-        let mut apoint3d: vecs::Vec3 = (10.0, 10.0, 10.0).into();
-        let mut camera3d: vecs::Vec3 = (1.0, 1.0, 1.0).into();
-        let camera_distance: f32 = 1.0;
-        let apoint2d: vecs::Vec2 = a3d_to_2d::a3d_to_2d(apoint3d, camera3d, camera_distance).unwrap();
-        println!("Point: {apoint2d:?}");
+        //let mut apoint3d: vecs::Vec3 = (10.0, 10.0, 10.0).into();
+        //let mut another_point3d: vecs::Vec3 = (10.0, 20.0, 10.0).into();
+        let floor_plan: Vec<(Vec2, Vec2)> = vec![
+            ((10.0, 10.0).into(), (10.0, 20.0).into()),
+        ];
 
+        let mut camera3d: vecs::Vec3 = Default::default();
+        let camera_distance: f32 = 100.0;
+
+        let mut parsed_walls = vec![];
+        let wall_points = floor_plan.iter().map(|(l, r)| wall_floor_to_3d(l, r));
+        for wall_point_set in wall_points {
+            let mut wall: Vec<Vec2> = vec![];
+            for wall_point in wall_point_set {
+                match a3d_to_2d::a3d_to_2d(wall_point, camera3d, camera_distance) {
+                    Some(s) => wall.push(Vec2 {
+                        x: s.x + 50.0,
+                        y: s.y + 50.0, 
+                    }),
+                    None => continue,
+                }
+            };
+            parsed_walls.push(Polygon::new(wall));
+        }
         GameState {
             // Initialize game state here
-            
-
-
+            camera3d,
+            walls: parsed_walls,
         }
     }
 }
@@ -49,7 +95,6 @@ impl event::EventHandler for GameState {
 fn draw_screen(game_state: &mut GameState, ctx: &mut Context, canvas: &mut graphics::Canvas) -> GameResult<()> {
     // Drawing logic here
 
-    let line_seg: LineSegment = (10.0, 10.0, 20.0, 20.0).into();
 
   /*   let line_seg = LineSegment {
         start: Vec2 {
@@ -71,9 +116,13 @@ fn draw_screen(game_state: &mut GameState, ctx: &mut Context, canvas: &mut graph
             y: 0.0,
         }
     };*/
-
-    line_seg.draw(ctx, canvas, Color::BLACK);
-    //line_seg2.draw(ctx, canvas, Color::BLACK);
+    for wall in game_state.walls[0].points.iter() {
+        println!("{} {}", wall.x, wall.y)
+    }
+    for wall in game_state.walls.iter(){
+        wall.draw_filled(ctx, canvas, Color::BLACK);
+    }
+    //line_seg.draw(ctx, canvas, Color::BLACK);
 
 
     Ok(())
