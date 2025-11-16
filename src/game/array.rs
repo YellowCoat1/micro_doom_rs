e395e1912c24cad03315d39b3f6cc2927a5bfcb4
Lynx/Jason <jasond3663@gmail.com>
2025-11-15 22:04:31 -0800

    rotation

--- src/game/array.rs
+++ src/game/array.rs
@@ -1,3 +1,13 @@
+use super::vecs::Vec2;
+use nalgebra_glm as glm;
+use glm::DVec2;
+
 pub struct Matrix2 {
     data: [[f64; 2]; 2],
+}
+
+pub fn ror_vec2(vec: Vec2, r: f32) -> Vec2 {
+    let x = vec.x * r.cos() - vec.y * r.sin();
+    let y = vec.x * r.sin() + vec.y * r.cos();
+    (x,y).into()
 }
