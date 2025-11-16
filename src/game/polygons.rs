e6e317836d525fbeeaa42c4e655cb3b61d745ded
Lynx/Jason <jasond3663@gmail.com>
2025-11-15 16:43:41 -0800

    Drawing polygons

--- src/game/polygons.rs
+++ src/game/polygons.rs
@@ -1,3 +1,5 @@
+use ggez::{Context, graphics::{Canvas, Color}};
+
 use super::vecs::Vec2;
 
 pub struct Polygon {
@@ -7,5 +9,14 @@ pub struct Polygon {
 impl Polygon {
     pub fn new(points: Vec<Vec2>) -> Self {
         Self { points }
+    }
+    pub fn draw(&self, ctx: &mut Context, canvas: &mut Canvas, color: Color) {
+        let poly = ggez::graphics::Mesh::new_polygon(
+            ctx,
+            ggez::graphics::DrawMode::fill(),
+            &self.points.iter().map(|p| ggez::mint::Point2 { x: p.x, y: p.y }).collect::<Vec<_>>(),
+            color,
+        ).unwrap();
+        canvas.draw(&poly, ggez::graphics::DrawParam::default())
     }
 }
