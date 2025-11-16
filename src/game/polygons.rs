90b56a48b42da546f8cbfd918812008f6c715fcd
Lynx/Jason <jasond3663@gmail.com>
2025-11-15 16:45:27 -0800

    Polygon impls

--- src/game/polygons.rs
+++ src/game/polygons.rs
@@ -6,6 +6,7 @@ pub struct Polygon {
     pub points: Vec<Vec2>,
 }
 
+
 impl Polygon {
     pub fn new(points: Vec<Vec2>) -> Self {
         Self { points }
@@ -18,5 +19,17 @@ impl Polygon {
             color,
         ).unwrap();
         canvas.draw(&poly, ggez::graphics::DrawParam::default())
+    }
+}
+
+impl From<&[Vec2]> for Polygon {
+    fn from(points: &[Vec2]) -> Self {
+        Self::new(points.to_vec())
+    }
+}
+
+impl From<Vec<Vec2>> for Polygon {
+    fn from(points: Vec<Vec2>) -> Self {
+        Self::new(points)
     }
 }
