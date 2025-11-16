883b1776f5c6c70e08508877b85f53d045e61572
Lynx/Jason <jasond3663@gmail.com>
2025-11-15 17:04:15 -0800

    unfilled polys

--- src/game/polygons.rs
+++ src/game/polygons.rs
@@ -11,10 +11,19 @@ impl Polygon {
     pub fn new(points: Vec<Vec2>) -> Self {
         Self { points }
     }
-    pub fn draw(&self, ctx: &mut Context, canvas: &mut Canvas, color: Color) {
+    pub fn draw_filled(&self, ctx: &mut Context, canvas: &mut Canvas, color: Color) {
         let poly = ggez::graphics::Mesh::new_polygon(
             ctx,
             ggez::graphics::DrawMode::fill(),
+            &self.points.iter().map(|p| ggez::mint::Point2 { x: p.x, y: p.y }).collect::<Vec<_>>(),
+            color,
+        ).unwrap();
+        canvas.draw(&poly, ggez::graphics::DrawParam::default())
+    }
+    pub fn draw_unfilled(&self, ctx: &mut Context, canvas: &mut Canvas, color: Color) {
+        let poly = ggez::graphics::Mesh::new_polygon(
+            ctx,
+            ggez::graphics::DrawMode::stroke(1.5),
             &self.points.iter().map(|p| ggez::mint::Point2 { x: p.x, y: p.y }).collect::<Vec<_>>(),
             color,
         ).unwrap();
